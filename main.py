50e6fa6763eecbd428bf41b76553c586db035896
Andres Matoma <andresmagic17@gmail.com>
2026-09-16 09:59:18 -0500

    Agrega verificación de contraseña para logística

--- main.py
+++ main.py
@@ -1,46 +1,44 @@
-from funciones import verificationPassword #Acá lo que hacemos es llamar al función creada para la contraseña, primero va el 
-#nombre de la hoja donde están las funciones y luego el nombre de la funcuón (de la hoja fuciones porte la función verif...)
+from funciones import * #Cuando se usa import * se esta importando TODAS las funciones que estan en el archivo sin necesidad de estar llamandolas una por una ;p
 
 passwordAdmin = "Caremonda"
 passwordCajero = "1245"
 passwordLogistica = "Nose"
 passwordDomiciliario = "EsRappi"
-intentos = 3 #ponemos la cantidad de intentos que tiene el usario para poner su contraseña correctamente. 
+intentos = 3 
 
 while True:
     print ("\n" + "=" * 30)  
-    print ("SELECCIONE SU ROLL".center(30))
+    print ("SELECCIONE SU ROL".center(30))
     print ("="*30)
-
     print("\n1. ADMINISTRADOR")
     print("2. CAJERO")
     print("3. LOGISTICA")
     print("4. DOMICILIARIO")
-
     print("\n5. SALIR")
-
     while True:
         try:
-            seleccion = int(input("INGRSE SU ROLL: "))
+            seleccion = int(input("\nINGRESE SU ROL: "))
             break
         except ValueError:
                 print("Ingrese una opción válida: ")
-
+                
     if seleccion == 5: 
-         exit("Tenga buen día")
+         exit("Tenga buen día...")
     else: 
          match seleccion:
               case 1: #ADMIN
-                         if verificationPassword (passwordAdmin, intentos):
-                                                          print("Bienvenido, ADMIN")
-#El error que estaba presentado, era que no se repetía el código cuando se ponia mal la contraseña. Se resolvió en la
-# def, el return false, estaba dentro del for            
+                if verificationPassword (passwordAdmin, intentos):
+                    print("\nBienvenido, ADMIN")
+                    menuAdmin()                            
               case 2: #CAJERO
-                         if verificationPassword (passwordCajero, intentos):
-                                                          print("Bienvenido, CAJERO") 
+                 if verificationPassword (passwordCajero, intentos):
+                    print("\nBienvenido, CAJERO")
+                    menuCajero()  
               case 3: #LOGISTICA
-                         if verificationPassword (passwordLogistica, intentos):
-                                                          print("Bienvenido, LOGISTICA") 
+                if verificationPassword (passwordLogistica, intentos):
+                    print("\nBienvenido, LOGISTICA")
+                    menuLogistica()
               case 4: #DOMICILIARIO
-                         if verificationPassword (passwordDomiciliario, intentos):
-                                                          print("Bienvenido, DOMICILIARIO") 
+                 if verificationPassword (passwordDomiciliario, intentos):
+                    print("\nBienvenido, DOMICILIARIO")
+                    menuDomiciliario()
